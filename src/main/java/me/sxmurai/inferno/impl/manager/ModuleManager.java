3eab3799fc6710215452ce60c89c4cbff2c2690c
Sxmurai <gavrw0607@gmail.com>
2021-11-16 20:40:24 -0600

    (impl): AntiVoid

--- src/main/java/me/sxmurai/inferno/impl/manager/ModuleManager.java
+++ src/main/java/me/sxmurai/inferno/impl/manager/ModuleManager.java
@@ -51,6 +51,7 @@ public class ModuleManager {
                 new Velocity(),
 
                 // player
+                new AntiVoid(),
                 new FastUse(),
                 new HotbarRefill(),
                 new MultiTask(),
