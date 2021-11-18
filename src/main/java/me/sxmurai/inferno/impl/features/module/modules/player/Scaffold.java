8c804e871fed3d732feea9ce7eec96ea84cb986b
Sxmurai <gavrw0607@gmail.com>
2021-11-17 20:39:57 -0600

    (refactor): Scaffold rewrite

--- src/main/java/me/sxmurai/inferno/impl/features/module/modules/player/Scaffold.java
+++ src/main/java/me/sxmurai/inferno/impl/features/module/modules/player/Scaffold.java
@@ -1,10 +1,10 @@
 package me.sxmurai.inferno.impl.features.module.modules.player;
 
-import me.sxmurai.inferno.util.world.BlockUtil;
-import me.sxmurai.inferno.util.entity.InventoryUtil;
-import me.sxmurai.inferno.util.timing.Timer;
 import me.sxmurai.inferno.impl.features.module.Module;
 import me.sxmurai.inferno.impl.option.Option;
+import me.sxmurai.inferno.util.entity.InventoryUtil;
+import me.sxmurai.inferno.util.timing.Timer;
+import me.sxmurai.inferno.util.world.BlockUtil;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -13,17 +13,8 @@ import net.minecraft.util.math.BlockPos;
 @Module.Define(name = "Scaffold", category = Module.Category.Player)
 @Module.Info(description = "Places blocks under your feet")
 public class Scaffold extends Module {
-    private static final BlockPos[] DIRECTION_OFFSETS = new BlockPos[] {
-            new BlockPos(0, 0, 0),
-            new BlockPos(-1, 0, 0),
-            new BlockPos(1, 0, 0),
-            new BlockPos(0, 0, -1),
-            new BlockPos(0, 0, 1)
-    };
-
     public final Option<Boolean> tower = new Option<>("Tower", true);
-    public final Option<Boolean> offhand = new Option<>("Offhand", true);
-    public final Option<InventoryUtil.Swap> switchTo = new Option<>("Swap", InventoryUtil.Swap.Legit);
+    public final Option<InventoryUtil.Swap> swap = new Option<>("Swap", InventoryUtil.Swap.Legit);
     public final Option<Place> place = new Option<>("Place", Place.Vanilla);
     public final Option<Boolean> rotate = new Option<>("Rotate", true);
     public final Option<Boolean> swing = new Option<>("Swing", true);
@@ -33,30 +24,41 @@ public class Scaffold extends Module {
 
     @Override
     public void onUpdate() {
-        BlockPos origin = new BlockPos(mc.player.posX, mc.player.posY - 1.0, mc.player.posZ);
-        if (mc.world.isAirBlock(origin)) {
-            BlockPos next = this.getNextPlacePos(origin);
-            if (next == null) {
-                return;
-            }
-
-            int slot = InventoryUtil.getHotbarItemSlot(ItemBlock.class, this.offhand.getValue());
-            if (slot == -1) {
-                return;
-            }
-
+        BlockPos below = new BlockPos(mc.player.posX, mc.player.posY - 1.0, mc.player.posZ);
+        if (mc.world.isAirBlock(below)) {
+            EnumHand hand;
             int oldSlot = -1;
-            EnumHand hand = slot == InventoryUtil.OFFHAND_SLOT ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND;
-            if (hand == EnumHand.MAIN_HAND) {
-                oldSlot = mc.player.inventory.currentItem;
-                InventoryUtil.swap(slot, this.switchTo.getValue());
+            if (InventoryUtil.isHolding(ItemBlock.class, true)) {
+                hand = InventoryUtil.getHeld(EnumHand.MAIN_HAND).getItem() instanceof ItemBlock ? EnumHand.MAIN_HAND : InventoryUtil.getHeld(EnumHand.OFF_HAND).getItem() instanceof ItemBlock ? EnumHand.OFF_HAND : null;
+            } else {
+                if (this.swap.getValue() == InventoryUtil.Swap.None) {
+                    return;
+                }
+
+                int slot = InventoryUtil.getHotbarItemSlot(ItemBlock.class, true);
+                if (slot == -1) {
+                    return;
+                }
+
+                hand = slot == 45 ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND;
+                if (hand == EnumHand.MAIN_HAND) {
+                    oldSlot = mc.player.inventory.currentItem;
+                    InventoryUtil.swap(slot, this.swap.getValue());
+                }
             }
 
-            mc.player.setActiveHand(hand);
+            if (hand == null) {
+                return;
+            }
 
-            BlockUtil.place(next, hand, this.place.getValue() == Place.Packet, this.sneak.getValue(), this.swing.getValue(), this.rotate.getValue());
+            EnumFacing direction = BlockUtil.getFacing(below);
+            if (direction == null) {
+                return;
+            }
 
-            if (this.tower.getValue() && next.equals(origin) && !mc.world.isAirBlock(next) && mc.gameSettings.keyBindJump.isKeyDown()) {
+            BlockUtil.place(below, hand, this.place.getValue() == Place.Packet, this.sneak.getValue(), this.swing.getValue(), this.rotate.getValue());
+
+            if (!mc.world.isAirBlock(below) && mc.gameSettings.keyBindJump.isKeyDown() && direction == EnumFacing.DOWN && this.tower.getValue()) {
                 mc.player.motionX *= 0.3;
                 mc.player.motionZ *= 0.3;
                 mc.player.jump();
@@ -68,35 +70,9 @@ public class Scaffold extends Module {
             }
 
             if (oldSlot != -1) {
-                InventoryUtil.swap(oldSlot, this.switchTo.getValue());
+                InventoryUtil.swap(oldSlot, this.swap.getValue());
             }
         }
-    }
-
-    private BlockPos getNextPlacePos(BlockPos origin) {
-        if (!mc.world.isAirBlock(origin)) {
-            return null;
-        }
-
-        for (BlockPos offset : Scaffold.DIRECTION_OFFSETS) {
-            for (EnumFacing direction : EnumFacing.values()) {
-                if (direction == EnumFacing.UP) {
-                    continue;
-                }
-
-                if (direction == EnumFacing.DOWN) {
-                    return origin;
-                } else {
-                    return origin.add(offset);
-                }
-            }
-        }
-
-        return null;
-    }
-
-    public enum Switch {
-        Legit, Silent
     }
 
     public enum Place {
