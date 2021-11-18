650712db2a9e8912fb024a91a35e465b2e4a3921
Sxmurai <gavrw0607@gmail.com>
2021-11-17 20:09:51 -0600

    (refactor): Cleanup a bit

--- src/main/java/me/sxmurai/inferno/impl/features/module/modules/combat/Critcals.java
+++ src/main/java/me/sxmurai/inferno/impl/features/module/modules/combat/Critcals.java
@@ -9,32 +9,28 @@ import net.minecraft.network.play.client.CPacketUseEntity;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module.Define(name = "Criticals", category = Module.Category.Combat)
-@Module.Info(description = "Applies critical hits automatically")
+@Module.Info(description = "Scores some critical hits for you")
 public class Critcals extends Module {
     public final Option<Mode> mode = new Option<>("Mode", Mode.Packet);
-    public final Option<Boolean> safe = new Option<>("Safe", false);
 
     @SubscribeEvent
     public void onPacketSend(PacketEvent.Send event) {
-        if (fullNullCheck() && event.getPacket() instanceof CPacketUseEntity) {
-            CPacketUseEntity packet = (CPacketUseEntity) event.getPacket();
-            if (
-                    packet.getAction() != CPacketUseEntity.Action.ATTACK ||
-                            (this.safe.getValue() && !(packet.getEntityFromWorld(mc.world) instanceof EntityLivingBase)) || !mc.player.onGround) {
+        if (event.getPacket() instanceof CPacketUseEntity) {
+            CPacketUseEntity packet = event.getPacket();
+            if (packet.getAction() != CPacketUseEntity.Action.ATTACK || !(packet.getEntityFromWorld(mc.world) instanceof EntityLivingBase) || !mc.player.onGround) {
                 return;
             }
 
             switch (this.mode.getValue()) {
                 case Packet: {
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
                     mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.3, mc.player.posZ, false));
                     mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
                     break;
                 }
 
                 case NCPStrict: {
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.062602401692772D, mc.player.posZ, false));
-                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.0726023996066094D, mc.player.posZ, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.062602401692772, mc.player.posZ, false));
+                    mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY + 0.0726023996066094, mc.player.posZ, false));
                     mc.player.connection.sendPacket(new CPacketPlayer.Position(mc.player.posX, mc.player.posY, mc.player.posZ, false));
                     break;
                 }
