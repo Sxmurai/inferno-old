12f2eb98c9578ec4ab38de07c92a66245e5ef596
Sxmurai <gavrw0607@gmail.com>
2021-11-18 19:02:08 -0600

    (impl): Fix versioning to match SemVer standards

--- src/main/java/me/sxmurai/inferno/Inferno.java
+++ src/main/java/me/sxmurai/inferno/Inferno.java
@@ -15,7 +15,7 @@ import org.lwjgl.opengl.Display;
 public class Inferno {
     public static final String NAME = "Inferno";
     public static final String ID = "inferno";
-    public static final String VERSION = "b1.1.0";
+    public static final String VERSION = "1.1.0-beta";
 
     @Mod.Instance
     public static Inferno INSTANCE;
