373da6c1d18f719cffb5538e1ef8cc15bc819c42
Sxmurai <gavrw0607@gmail.com>
2021-12-05 22:43:42 -0600

    (impl): Remove useless options from AutoBowRelease

--- src/main/java/cope/inferno/impl/features/module/modules/combat/AutoBowRelease.java
+++ src/main/java/cope/inferno/impl/features/module/modules/combat/AutoBowRelease.java
@@ -4,17 +4,12 @@ import cope.inferno.Inferno;
 import cope.inferno.impl.features.module.Module;
 import cope.inferno.impl.settings.Setting;
 import cope.inferno.util.entity.InventoryUtil;
-import cope.inferno.util.timing.TickTimer;
 import net.minecraft.init.Items;
 
 @Module.Define(name = "AutoBowRelease", category = Module.Category.Combat)
 @Module.Info(description = "Automatically releases your bow for you")
 public class AutoBowRelease extends Module {
-    public final Setting<Boolean> offhand = new Setting<>("Offhand", true);
     public final Setting<Integer> amount = new Setting<>("Amount", 4, 1, 30);
-    public final Setting<Integer> delay = new Setting<>("Delay", 1, 0, 10);
-
-    private final TickTimer timer = new TickTimer();
 
     @Override
     public String getDisplayInfo() {
@@ -22,13 +17,9 @@ public class AutoBowRelease extends Module {
     }
 
     @Override
-    public void onTick() {
-        if (this.timer.passed(this.delay.getValue())) {
-            this.timer.reset();
-
-            if (InventoryUtil.isHolding(Items.BOW, this.offhand.getValue()) && mc.player.getItemInUseMaxCount() >= this.amount.getValue()) {
-                Inferno.interactionManager.releaseItem();
-            }
+    public void onUpdate() {
+        if (InventoryUtil.isHolding(Items.BOW, true) && mc.player.getItemInUseMaxCount() >= this.amount.getValue()) {
+            Inferno.interactionManager.releaseItem();
         }
     }
 }
